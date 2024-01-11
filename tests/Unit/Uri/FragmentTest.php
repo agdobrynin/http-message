113b7a165a83b776366d4f0bd467f17f63aafaf6
Alexey Dobrynin <ag.dobrynin@gmail.com>
2024-01-11 11:14:03 +0400

    test: [Uri] Update test fo URI fragment.

--- tests/Unit/Uri/FragmentTest.php
+++ tests/Unit/Uri/FragmentTest.php
@@ -4,9 +4,44 @@ declare(strict_types=1);
 
 namespace Tests\Kaspi\HttpMessage\Unit\Uri;
 
+use Kaspi\HttpMessage\Uri;
+use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 
 /**
  * @internal
  */
-class FragmentTest extends TestCase {}
+#[CoversClass(Uri::class)]
+class FragmentTest extends TestCase
+{
+    public static function dataFragmentInConstructor(): \Generator
+    {
+        yield 'empty' => [
+            'uri' => '',
+            'fragment' => '',
+        ];
+
+        yield 'has host and fragment' => [
+            'uri' => 'localhost/#frg-20',
+            'fragment' => 'frg-20',
+        ];
+
+        yield 'no host but has fragment style string' => [
+            'uri' => 'index.html#frg-20',
+            'fragment' => 'frg-20',
+        ];
+
+        yield 'has uri without fragment' => [
+            'uri' => 'HTTPS://domain.com/index.html',
+            'fragment' => '',
+        ];
+    }
+
+    /**
+     * @dataProvider dataFragmentInConstructor
+     */
+    public function testFragmentInConstructor(string $uri, string $fragment): void
+    {
+        $this->assertEquals($fragment, (new Uri($uri))->getFragment());
+    }
+}
--- tests/Unit/Uri/QueryTest.php
+++ tests/Unit/Uri/QueryTest.php
@@ -4,9 +4,29 @@ declare(strict_types=1);
 
 namespace Tests\Kaspi\HttpMessage\Unit\Uri;
 
+use Kaspi\HttpMessage\Uri;
+use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 
 /**
  * @internal
  */
-class QueryTest extends TestCase {}
+#[CoversClass(Uri::class)]
+class QueryTest extends TestCase
+{
+    public static function dataQueryInConstructor(): \Generator
+    {
+        yield 'empty' => [
+            'uri' => '',
+            'query' => '',
+        ];
+    }
+
+    /**
+     * @dataProvider dataQueryInConstructor
+     */
+    public function testQueryInConstructor(string $uri, string $query): void
+    {
+        $this->assertEquals($query, (new Uri($uri))->getQuery());
+    }
+}
