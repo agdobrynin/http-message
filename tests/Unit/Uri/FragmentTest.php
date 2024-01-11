08f411d9b1de2d2acf6b23f76fe840cafabd3808
Alexey Dobrynin <ag.dobrynin@gmail.com>
2024-01-11 11:33:40 +0400

    test: [Uri] Update test fo URI fragment.

--- tests/Unit/Uri/FragmentTest.php
+++ tests/Unit/Uri/FragmentTest.php
@@ -44,4 +44,42 @@ class FragmentTest extends TestCase
     {
         $this->assertEquals($fragment, (new Uri($uri))->getFragment());
     }
+
+    public static function dataWithFragment(): \Generator
+    {
+        yield 'empty' => [
+            'uri' => new Uri(''),
+            'fragment' => '',
+            'expect' => '',
+        ];
+
+        yield 'exist uri and set empty fragment' => [
+            'uri' => new Uri('https://www.com/document#frag*'),
+            'fragment' => '',
+            'expect' => '',
+        ];
+
+        yield 'uri and set fragment unavailable symbols' => [
+            'uri' => new Uri('https://www.com/document'),
+            'fragment' => '€ [евро]',
+            'expect' => '%E2%82%AC%20%5B%D0%B5%D0%B2%D1%80%D0%BE%5D',
+        ];
+
+        yield 'uri and set fragment available symbols' => [
+            'uri' => new Uri('https://www.com/document'),
+            'fragment' => 'azAF0~7.!-$_&\'(*)+,;=:@?/%E2%82%AC',
+            'expect' => 'azAF0~7.!-$_&\'(*)+,;=:@?/%E2%82%AC',
+        ];
+    }
+
+    /**
+     * @dataProvider dataWithFragment
+     */
+    public function testWithFragment(Uri $uri, string $fragment, string $expect): void
+    {
+        $new = $uri->withFragment($fragment);
+
+        $this->assertNotSame($new, $uri);
+        $this->assertEquals($expect, $new->getFragment());
+    }
 }
