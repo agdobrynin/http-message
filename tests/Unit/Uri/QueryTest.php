ed7e5a4ce52b1b34a9d5890d80d2518b378b3cab
Alexey Dobrynin <ag.dobrynin@gmail.com>
2024-01-11 12:29:51 +0400

    test: [Uri] Update test fo URI query.

--- tests/Unit/Uri/QueryTest.php
+++ tests/Unit/Uri/QueryTest.php
@@ -20,6 +20,21 @@ class QueryTest extends TestCase
             'uri' => '',
             'query' => '',
         ];
+
+        yield 'has host and query' => [
+            'uri' => 'localhost/?p1=10&x[]=10',
+            'query' => 'p1=10&x[]=10',
+        ];
+
+        yield 'no host but has query style string' => [
+            'uri' => '?param=abc&host=10',
+            'query' => 'param=abc&host=10',
+        ];
+
+        yield 'has uri without query string' => [
+            'uri' => 'HTTPS://domain.com/index.html',
+            'query' => '',
+        ];
     }
 
     /**
@@ -28,5 +43,44 @@ class QueryTest extends TestCase
     public function testQueryInConstructor(string $uri, string $query): void
     {
         $this->assertEquals($query, (new Uri($uri))->getQuery());
+    }
+
+    public static function dataWithQuery(): \Generator
+    {
+        yield 'empty' => [
+            'uri' => new Uri(''),
+            'query' => '',
+            'expect' => '',
+        ];
+
+        yield 'exist uri and set empty query' => [
+            'uri' => new Uri('https://www.com/document?abc=10&x[]=fix%20plan'),
+            'query' => '',
+            'expect' => '',
+        ];
+
+        yield 'uri and set query unavailable symbols' => [
+            'uri' => new Uri('https://www.com/document'),
+            // TODO how to fix square brace
+            'query' => 'param money[]=€',
+            'expect' => 'param%20money%5B%5D=%E2%82%AC',
+        ];
+
+        yield 'uri and set fragment available symbols' => [
+            'uri' => new Uri('https://www.com/document'),
+            'query' => 'azAF0~7.!-$_&\'(*)+,;=:@?/%E2%82%AC',
+            'expect' => 'azAF0~7.!-$_&\'(*)+,;=:@?/%E2%82%AC',
+        ];
+    }
+
+    /**
+     * @dataProvider dataWithQuery
+     */
+    public function testWithQuery(Uri $uri, string $query, string $expect): void
+    {
+        $new = $uri->withQuery($query);
+
+        $this->assertNotSame($new, $uri);
+        $this->assertEquals($expect, $new->getQuery());
     }
 }
