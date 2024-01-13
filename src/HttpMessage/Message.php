e93514657d8f9d2c00a3220cafcea605a7d1f1cc
Alexey Dobrynin <ag.dobrynin@gmail.com>
2024-01-13 20:06:39 +0400

    feat: [Message] Implementation of Psr\Http\Message\MessageInterface

--- src/HttpMessage/Message.php
+++ src/HttpMessage/Message.php
@@ -9,58 +9,183 @@ use Psr\Http\Message\StreamInterface;
 
 class Message implements MessageInterface
 {
+    private string $version = '1.1';
+    private array $headers = [];
+    private StreamInterface $body;
+
     public function getProtocolVersion(): string
     {
-        // TODO: Implement getProtocolVersion() method.
+        return $this->version;
     }
 
-    public function withProtocolVersion(string $version): MessageInterface
+    /**
+     * @phan-suppress PhanParamSignatureRealMismatchReturnType
+     */
+    public function withProtocolVersion(string $version): static
     {
-        // TODO: Implement withProtocolVersion() method.
+        if (\preg_match('/^\d+\.\d+$/', $version)) {
+            $new = clone $this;
+            $new->version = $version;
+
+            return $new;
+        }
+
+        throw new \InvalidArgumentException('Protocol must be implement "<major>.<minor>" numbering scheme');
     }
 
     public function getHeaders(): array
     {
-        // TODO: Implement getHeaders() method.
+        return $this->headers;
     }
 
     public function hasHeader(string $name): bool
     {
-        // TODO: Implement hasHeader() method.
+        if ('' === $name) {
+            throw new \InvalidArgumentException('Header name is empty string');
+        }
+
+        return \array_key_exists(\strtolower($name), $this->headers);
     }
 
     public function getHeader(string $name): array
     {
-        // TODO: Implement getHeader() method.
+        if (!$this->hasHeader($name)) {
+            return [];
+        }
+
+        return $this->headers[\strtolower($name)];
     }
 
     public function getHeaderLine(string $name): string
     {
-        // TODO: Implement getHeaderLine() method.
+        return \implode(', ', $this->getHeader($name));
     }
 
-    public function withHeader(string $name, $value): MessageInterface
+    /**
+     * @phan-suppress PhanParamSignatureRealMismatchReturnType
+     */
+    public function withHeader(string $name, mixed $value): static
     {
-        // TODO: Implement withHeader() method.
+        $normalizedName = \strtolower($name);
+        $hasHeader = $this->hasHeader($normalizedName);
+        $value = $this->validateRFC7230AndTrim($name, $value);
+        $new = clone $this;
+
+        if ($hasHeader) {
+            unset($this->headers[$normalizedName]);
+        }
+
+        $new->headers[$normalizedName] = $value;
+
+        return $new;
     }
 
-    public function withAddedHeader(string $name, $value): MessageInterface
+    /**
+     * @phan-suppress PhanParamSignatureRealMismatchReturnType
+     */
+    public function withAddedHeader(string $name, mixed $value): static
     {
-        // TODO: Implement withAddedHeader() method.
+        $normalizedName = \strtolower($name);
+        $hasHeader = $this->hasHeader($normalizedName);
+        $value = $this->validateRFC7230AndTrim($name, $value);
+
+        $new = clone $this;
+
+        if ($hasHeader) {
+            $new->headers[$normalizedName] = \array_merge($this->headers[$normalizedName], $value);
+        } else {
+            $new->headers[$normalizedName] = $value;
+        }
+
+        return $new;
     }
 
-    public function withoutHeader(string $name): MessageInterface
+    /**
+     * @phan-suppress PhanParamSignatureRealMismatchReturnType
+     */
+    public function withoutHeader(string $name): static
     {
-        // TODO: Implement withoutHeader() method.
+        $normalizedName = \strtolower($name);
+
+        if (!$this->hasHeader($normalizedName)) {
+            return $this;
+        }
+
+        $new = clone $this;
+        unset($new->headers[$normalizedName]);
+
+        return $new;
     }
 
     public function getBody(): StreamInterface
     {
-        // TODO: Implement getBody() method.
+        if (!isset($this->body)) {
+            $this->body = new Stream('');
+        }
+
+        return $this->body;
     }
 
-    public function withBody(StreamInterface $body): MessageInterface
+    /**
+     * @phan-suppress PhanParamSignatureRealMismatchReturnType
+     */
+    public function withBody(StreamInterface $body): static
     {
-        // TODO: Implement withBody() method.
+        $new = clone $this;
+        $new->body = $body;
+
+        return $new;
+    }
+
+    /**
+     *  Header
+     *  -------
+     *  token =  1*tchar
+     *  tchar =  "!" / "#" / "$" / "%" / "&" / "'" / "*" / "+" / "-" / "." / "^" / "_" / "`" / "|" / "~"
+     *           / DIGIT / ALPHA
+     *           ; any VCHAR, except delimiters.
+     *
+     *  Value of header
+     *  -----
+     *  A string of text is parsed as a single value if it is quoted using
+     *  double-quote marks.
+     *
+     *  quoted-string = DQUOTE *( qdtext / quoted-pair ) DQUOTE
+     *  qdtext        = HTAB / SP /%x21 / %x23-5B / %x5D-7E / obs-text
+     *  obs-text      = %x80-FF
+     *  quoted-pair   = "\" ( HTAB / SP / VCHAR / obs-text )
+     *
+     * @see https://datatracker.ietf.org/doc/html/rfc7230#section-3.2
+     * @see https://datatracker.ietf.org/doc/html/rfc7230#section-3.2.6
+     */
+    private function validateRFC7230AndTrim(string $header, mixed $values): array
+    {
+        if ('' === $header
+            || 1 !== \preg_match('/^[!#$%&\'*+.^_`|~0-9A-Za-z-]+$/D', $header)) {
+            throw new \InvalidArgumentException('Header name must be RFC 7230 compatible');
+        }
+
+        $valuesRaw = \array_filter(!\is_array($values) ? [$values] : $values);
+
+        if ([] === $valuesRaw) {
+            throw new \InvalidArgumentException('Header values must be non empty string');
+        }
+
+        $result = [];
+
+        foreach ($valuesRaw as $value) {
+            if ((!\is_numeric($value) && !\is_string($value))
+                || 1 !== \preg_match('/^[ \t\x21\x23-\x5B\x5D-\x7E\x80-\xFF]*$/', (string) $value)) {
+                $val = \var_export($value, true);
+
+                throw new \InvalidArgumentException('Header value must be RFC 7230 compatible. Got: '.$val);
+            }
+
+            $result[] = ($v = \trim((string) $value, " \t"))
+                ? $v
+                : throw new \InvalidArgumentException('Header values must be a non empty string');
+        }
+
+        return $result;
     }
 }
