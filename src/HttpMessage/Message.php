c6d12972aceb1e75d3556674aeb7d04d7a43253f
Alexey Dobrynin <ag.dobrynin@gmail.com>
2024-01-13 12:17:57 +0400

    feat: [Message] Add class with stub methods.

---
+++ src/HttpMessage/Message.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kaspi\HttpMessage;
+
+use Psr\Http\Message\MessageInterface;
+use Psr\Http\Message\StreamInterface;
+
+class Message implements MessageInterface
+{
+    public function getProtocolVersion(): string
+    {
+        // TODO: Implement getProtocolVersion() method.
+    }
+
+    public function withProtocolVersion(string $version): MessageInterface
+    {
+        // TODO: Implement withProtocolVersion() method.
+    }
+
+    public function getHeaders(): array
+    {
+        // TODO: Implement getHeaders() method.
+    }
+
+    public function hasHeader(string $name): bool
+    {
+        // TODO: Implement hasHeader() method.
+    }
+
+    public function getHeader(string $name): array
+    {
+        // TODO: Implement getHeader() method.
+    }
+
+    public function getHeaderLine(string $name): string
+    {
+        // TODO: Implement getHeaderLine() method.
+    }
+
+    public function withHeader(string $name, $value): MessageInterface
+    {
+        // TODO: Implement withHeader() method.
+    }
+
+    public function withAddedHeader(string $name, $value): MessageInterface
+    {
+        // TODO: Implement withAddedHeader() method.
+    }
+
+    public function withoutHeader(string $name): MessageInterface
+    {
+        // TODO: Implement withoutHeader() method.
+    }
+
+    public function getBody(): StreamInterface
+    {
+        // TODO: Implement getBody() method.
+    }
+
+    public function withBody(StreamInterface $body): MessageInterface
+    {
+        // TODO: Implement withBody() method.
+    }
+}
