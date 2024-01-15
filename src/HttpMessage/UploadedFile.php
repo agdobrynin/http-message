7d77bfac867d6801638406b33209a4f4804f8657
Alexey Dobrynin <ag.dobrynin@gmail.com>
2024-01-15 15:32:37 +0400

    feat: [UploadedFile] Implementation of UploadedFileInterface.

--- src/HttpMessage/UploadedFile.php
+++ src/HttpMessage/UploadedFile.php
@@ -9,55 +9,154 @@ use Psr\Http\Message\UploadedFileInterface;
 
 class UploadedFile implements UploadedFileInterface
 {
-    public function __construct()
-    {
-    }
+    private const UPLOAD_ERR = [
+        \UPLOAD_ERR_OK,
+        \UPLOAD_ERR_INI_SIZE,
+        \UPLOAD_ERR_FORM_SIZE,
+        \UPLOAD_ERR_PARTIAL,
+        \UPLOAD_ERR_NO_FILE,
+        \UPLOAD_ERR_NO_TMP_DIR,
+        \UPLOAD_ERR_CANT_WRITE,
+        \UPLOAD_ERR_EXTENSION,
+    ];
 
     /**
-     * @inheritDoc
+     * File with full path.
      */
+    private ?string $file;
+
+    /**
+     * Stream of file.
+     */
+    private ?StreamInterface $stream;
+
+    /**
+     * True then the file was successfully moved using UploadedFile::moveTo.
+     */
+    private bool $moved = false;
+
+    /**
+     * @param StreamInterface|string $fileOrStream    file with path or StreamInterface of uploaded file
+     * @param ?int                   $size            uploaded file size
+     * @param null|string            $originFileName  the value stored in the "name" key of the file in the $_FILES array
+     * @param null|string            $originMediaType the value stored in the "type" key of the file in the $_FILES array
+     */
+    public function __construct(
+        StreamInterface|string $fileOrStream,
+        private readonly int $error,
+        private readonly ?int $size = null,
+        private readonly ?string $originFileName = null,
+        private readonly ?string $originMediaType = null
+    ) {
+        if (!isset(self::UPLOAD_ERR[$this->error])) {
+            throw new \InvalidArgumentException('Invalid upload file error. Got: '.$this->error);
+        }
+
+        if (\UPLOAD_ERR_OK === $this->error) {
+            if ($fileOrStream instanceof StreamInterface) {
+                $this->stream = $fileOrStream;
+            } elseif ('' !== $fileOrStream) {
+                $this->file = $fileOrStream;
+            } else {
+                throw new \InvalidArgumentException(
+                    'Invalid parameter. "fileOrStream" must provide non-empty string or '.StreamInterface::class
+                );
+            }
+        }
+    }
+
     public function getStream(): StreamInterface
     {
-        // TODO: Implement getStream() method.
+        $this->isAvailable();
+
+        if ($this->stream) {
+            return $this->stream;
+        }
+
+        $resource = @\fopen($this->file, 'rb');
+
+        if (false === $resource) {
+            $error = \error_get_last()['message'] ?? '';
+
+            throw new \RuntimeException("Cannot open file {$this->file}  [{$error}]");
+        }
+
+        return new Stream($resource);
     }
 
-    /**
-     * @inheritDoc
-     */
     public function moveTo(string $targetPath): void
     {
-        // TODO: Implement moveTo() method.
+        $this->isAvailable();
+
+        if ('' === $targetPath) {
+            throw new \InvalidArgumentException('Target path must be provide non-empty string');
+        }
+
+        if ($this->file) {
+            $this->moved = 'cli' === \PHP_SAPI
+                ? @\rename($this->file, $targetPath)
+                : @\move_uploaded_file($this->file, $targetPath);
+
+            if (!$this->moved) {
+                $error = \error_get_last()['message'] ?? '';
+
+                throw new \RuntimeException("Cannot move uploaded file to {$targetPath} [{$error}]");
+            }
+        } else {
+            $resourceDest = @\fopen($targetPath, 'wb');
+
+            if (false === $resourceDest) {
+                $message = \error_get_last()['message'] ?? '';
+
+                throw new \RuntimeException("Cannot open target file {$targetPath} [{$message}]");
+            }
+
+            $dest = new Stream($resourceDest);
+
+            $from = $this->getStream();
+
+            if ($from->isSeekable()) {
+                $from->rewind();
+            }
+
+            while (!$from->eof()) {
+                if (!$dest->write($from->read(1048576))) {
+                    break;
+                }
+            }
+
+            $this->moved = true;
+        }
     }
 
-    /**
-     * @inheritDoc
-     */
     public function getSize(): ?int
     {
-        // TODO: Implement getSize() method.
+        return $this->size;
     }
 
-    /**
-     * @inheritDoc
-     */
     public function getError(): int
     {
-        // TODO: Implement getError() method.
+        return $this->error;
     }
 
-    /**
-     * @inheritDoc
-     */
     public function getClientFilename(): ?string
     {
-        // TODO: Implement getClientFilename() method.
+        return $this->originFileName;
     }
 
-    /**
-     * @inheritDoc
-     */
     public function getClientMediaType(): ?string
     {
-        // TODO: Implement getClientMediaType() method.
+        return $this->originMediaType;
+    }
+
+    private function isAvailable(): void
+    {
+        if (\UPLOAD_ERR_OK !== $this->error) {
+            throw new \RuntimeException('Upload file has error :'.$this->error);
+        }
+
+        if ($this->moved) {
+            throw new \RuntimeException('The uploaded file has already been moved');
+        }
     }
 }
