b8096a01f3b34e0b4120bcd035895c089ff51321
Alexey Dobrynin <ag.dobrynin@gmail.com>
2024-01-17 10:48:34 +0400

    feat: [Request] Implement __constructor, getRequestTarget, withRequestTarget, getMethod, withMethod, getUri.

--- src/HttpMessage/Request.php
+++ src/HttpMessage/Request.php
@@ -9,33 +9,64 @@ use Psr\Http\Message\UriInterface;
 
 class Request extends Message implements RequestInterface
 {
+    protected string $method;
+    protected UriInterface $uri;
+    protected ?string $requestTarget = null;
+
+    public function __construct(string $method, string|UriInterface $uri)
+    {
+        $this->method = $method;
+        $this->uri = \is_string($uri) ? new Uri($uri) : $uri;
+    }
+
     public function getRequestTarget(): string
     {
-        // TODO: Implement getRequestTarget() method.
+        if ($this->requestTarget) {
+            return $this->requestTarget;
+        }
+
+        $pathNormalize = ($path = $this->uri->getPath()) === ''
+            ? '/'
+            : $path;
+        $queryNormalize = ($query = $this->uri->getQuery()) === ''
+            ? $query
+            : '?'.$query;
+
+        return $pathNormalize.$queryNormalize;
     }
 
     public function withRequestTarget(string $requestTarget): RequestInterface
     {
-        // TODO: Implement withRequestTarget() method.
+        if (\str_contains($requestTarget, ' ')) {
+            throw new \InvalidArgumentException('Request target cannot contain whitespace');
+        }
+
+        $new = clone $this;
+        $new->requestTarget = $requestTarget;
+
+        return $new;
     }
 
     public function getMethod(): string
     {
-        // TODO: Implement getMethod() method.
+        return $this->method;
     }
 
     public function withMethod(string $method): RequestInterface
     {
-        // TODO: Implement withMethod() method.
+        $new = clone $this;
+        $new->method = $method;
+
+        return $new;
     }
 
     public function getUri(): UriInterface
     {
-        // TODO: Implement getUri() method.
+        return $this->uri;
     }
 
     public function withUri(UriInterface $uri, bool $preserveHost = false): RequestInterface
     {
-        // TODO: Implement withUri() method.
+        // TODO Implement later.
     }
 }
