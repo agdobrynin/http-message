6395dea4ef52cca15c2b01883017bf4d2ce78752
Alexey Dobrynin <ag.dobrynin@gmail.com>
2024-01-20 16:24:18 +0400

    refactor: [Stream] Make Trait for resource from string.

---
+++ src/HttpMessage/CreateResourceFromStringTrait.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kaspi\HttpMessage;
+
+trait CreateResourceFromStringTrait
+{
+    private function resourceFromString(string $body)
+    {
+        $resource = \fopen('php://temp', 'r+b') ?: throw new \RuntimeException('Cannot open stream [php://temp]');
+        \fwrite($resource, $body);
+        \fseek($resource, 0);
+
+        return $resource;
+    }
+}
